fec504b6b5089301b12f14713db2d014685f7460
Ricardo Figueiredo <ricardocastrofigueiredo@gmail.com>
2025-06-25 14:09:40 +0100

    add ribbon buttons; apply proportionality on zoom; add yrsa font

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,26 @@
+const SECTIONS = {
+  front: document.getElementById("front-section"),
+  map: document.getElementById("map-section"),
+  album: document.getElementById("album-section"),
+  stats: document.getElementById("stats-section"),
+  back: document.getElementById("back-section"),
+};
+
+const hideAllSections = () => {
+  for (const key in SECTIONS) SECTIONS[key].hidden = true;
+};
+
+const switchSection = (sectionName) => {
+  hideAllSections();
+  SECTIONS[sectionName].hidden = false;
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-  const app = document.getElementById("app");
-  const sections = {
-    front: document.getElementById("front-section"),
-    map: document.getElementById("map-section"),
-    album: document.getElementById("album-section"),
-    stats: document.getElementById("stats-section"),
-    back: document.getElementById("back-section"),
-  };
-  const navButtons = sections.front.querySelectorAll("button");
-  navButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      console.log(`Switching to ${button.className.split("-")[0]} section`);
-      sections.front.hidden = true;
-      sections[button.className.split("-")[0]].hidden = false;
-    });
-  });
+  /*  open with front section */
+  switchSection("front");
+
+  const ribbonButtons = document.querySelectorAll("button.ribbon");
+  for (const button of ribbonButtons) {
+    button.addEventListener("click", () => switchSection(button.value));
+  }
 });
